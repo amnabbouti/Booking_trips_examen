6c2a1282f530eb0b64c2ad8fdec5d5982746092a
Amine_Abbouti <amine.abbouti@hotmail.com>
2025-06-10 12:25:30 +0200

    Add authentication and protected routes for trips management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TripController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth routes
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Protected routes
+Route::get('/trips', [TripController::class, 'adminIndex'])
+    ->middleware('auth')
+    ->name('trips.index');
